be5219758eb181da9261a03c3851b8ca8ee5b943
Logan <73564929+larrythegnome@users.noreply.github.com>
2023-10-06 11:27:07 -0400

    Part 3

--- BMI_Calculator/MainWindow.xaml.cs
+++ BMI_Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace BMI_Calculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    [XmlRoot("BMI Calc", Namespace = "www.bmicalc.ninja")]
     public partial class MainWindow : Window
     {
-
+        public string FilePath = "../../../BMI Calculator/";
+        public string FileName = "yourBMI.xml";
         public class Customer
         {
+            [XmlAttribute("Last Name")]
             public string lastName { get; set; }
+            [XmlAttribute("First Name")]
             public string firstName { get; set; }
+            [XmlAttribute("Phone Number")]
             public string phoneNumber { get; set; }
-            public string heightInches { get; set; }
-            public string weightLbs { get; set; }
+            [XmlAttribute("Height")]
+            public int heightInches { get; set; }
+            [XmlAttribute("Weight")]
+            public int weightLbs { get; set; }
+            [XmlAttribute("Customer BMI")]
+            public int custBMI { get; set; }
+            [XmlAttribute("Status")]
             public string statusTitle { get; set; }
         }
 
@@ -58,12 +71,28 @@ namespace BMI_Calculator
             customer1.firstName = xFirstNameBox.Text;
             customer1.phoneNumber = xPhoneBox.Text;
 
+
             int currentWeight = 0;
             int currentheight = 0;
             Int32.TryParse(xWeightLbsBox.Text, out currentWeight);
             Int32.TryParse(xHeightInachesBox.Text, out currentheight);
             customer1.weightLbs = currentWeight;
             customer1.heightInches = currentheight;
+
+            int bmi;
+            bmi = 703 * customer1.weightLbs / (customer1.heightInches * customer1.heightInches);
+            customer1.custBMI = bmi;
+
+            string yourBMIStatus = "NA";
+            customer1.statusTitle = yourBMIStatus;
+
+            MessageBox.Show($"The Customer's name is {customer1.firstName} {customer1.lastName} and they can be reached at {customer1.phoneNumber}. They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {bmi}");
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+            XmlSerializer ser = new XmlSerializer(typeof(Customer));
+
+            ser.Serialize(writer, customer1);
+            writer.Close();
         }
     }
 }
